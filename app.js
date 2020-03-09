17c6ec29bb93df1ad6c31682608baeb2bd23ad63
Pratama Sumirat <akun.pratamasumirat@gmail.com>
2020-03-09 12:16:05 +0700

    test

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express()
+
+app.get('*', (req, res) => {
+    res.send('kwowkoakaow')
+})
+
+app.listen(3000, () => console.log(`Running on port 3000~`))
