8b3479e171ee8a229927edb69466289063fd88ff
Pratama Sumirat <akun.pratamasumirat@gmail.com>
2020-03-09 12:22:40 +0700

    res send

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('*', (req, res) => {
-    res.send('kwowkoakaow ini bener ga yaa')
+    res.send('coba merge conflict plss')
 })
 
 app.listen(3000, () => console.log(`Running on port 3000~`))
